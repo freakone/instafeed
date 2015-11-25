7ee1e968338076df5eca945ee8e6e83d958c19a0
Kamil Górski <kamil@freakone.pl>
2015-11-25 19:53:25 +0100

    continious slides

--- app/scripts/4.js
+++ app/scripts/4.js
@@ -1,33 +1,50 @@
 var currentSlide = 0;
 var slides = document.getElementsByName('slides');
 var slide_container = document.getElementById('instafeed_container');
-var previousSlide = slides.length;
+var slide_container_hidden = document.getElementById('instafeed_container_hidden');
 var feed = null;
 
-function nextPhoto() {
-  slides[previousSlide].classList.remove('slide-active');
-  slides[currentSlide].classList.add('slide-active');
-  previousSlide = currentSlide;
-  currentSlide++;
-  if (currentSlide === slides.length) {
-    currentSlide = 0;
-    slide_container.innerHTML = null;
-    feed = new Instafeed({
+function initInsta(){
+
+   feed = new Instafeed({
+
       get: 'tagged',
-      tagName: 'hi5monte',
+      tagName: 'meetatmonterail',
       clientId: '25a5f37f226940b29b4856329ea4e68e',
-      target: 'instafeed_container',
+      target: 'instafeed_container_hidden',
       template: '<div class="slide" name="slides"><img class="insta" src="{{image}}" /></div>',
       resolution: 'standard_resolution',
       sortBy: 'most-recent',
-      limit: 11,
+      limit: 10,
       error: function(error) {
         refreshSite();
-      },
+      },      
+      after: function() {
+        slide_container.innerHTML = slide_container_hidden.innerHTML;
+        slide_container_hidden.innerHTML = null;
+        currentSlide = 0;
+        nextPhoto();
 
-      after: updateSlides()
+      }
     });
+
     feed.run();
+}
+
+function nextPhoto() {
+
+  if(!slides || slides.length === 0)
+    return;
+
+  if (currentSlide >= slides.length) {    
+    feed.run();
+  }
+  else  {
+    for(var i = 0; i < slides.length; i++){
+      slides[i].classList.remove('slide-active');
+    }
+
+    slides[currentSlide++].classList.add('slide-active');
   }
 }
 
@@ -37,28 +54,11 @@ function refreshSite() {
 
 function startInstagram() {
 
-  feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'hi5monte',
-    clientId: '25a5f37f226940b29b4856329ea4e68e',
-    target: 'instafeed_container',
-    template: '<div class="slide" name="slides"><img class="insta" src="{{image}}" /></div>',
-    resolution: 'standard_resolution',
-    sortBy: 'most-recent',
-    limit: 11,
-
-    after: updateSlides()
-  });
-  feed.run();
-
+  initInsta();
   setInterval(function () {
     nextPhoto();
-  }, 2000);
+  }, 5000);
 }
 
 startInstagram();
 
-function updateSlides() {
-  slides = document.getElementsByName('slides');
-  previousSlide = slides.length;
-}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,31 +17,6 @@ function nextSlide() {
   }
 }
 
-function nextPhoto() {
-  slides[previousSlide].classList.remove('slide-active');
-  slides[currentSlide].classList.add('slide-active');
-  previousSlide = currentSlide;
-  currentSlide++;
-  feed.next();
-  if (currentSlide === slides.length) {
-    currentSlide = 0;
-    slide_container.innerHTML = null;
-    feed = new Instafeed({
-      get: 'tagged',
-      tagName: 'hi5monte',
-      clientId: '25a5f37f226940b29b4856329ea4e68e',
-      target: 'instafeed_container',
-      template: '<div class="slide" name="slides"><img class="insta" src="{{image}}" /></div>',
-      resolution: 'standard_resolution',
-      sortBy: 'most-recent',
-      limit: 4,
-
-      after: updateSlides()
-    });
-    feed.run();
-  }
-}
-
 function startSlider() {
   timers.nextSlideInterval = setInterval(function () {
     nextSlide();
@@ -54,27 +29,6 @@ function stopSlider() {
   element.parentNode.removeChild(element);
 }
 
-function startInstagram() {
-
-  feed = new Instafeed({
-    get: 'tagged',
-    tagName: 'hi5monte',
-    clientId: '25a5f37f226940b29b4856329ea4e68e',
-    target: 'instafeed_container',
-    template: '<div class="slide" name="slides"><img class="insta" src="{{image}}" /></div>',
-    resolution: 'standard_resolution',
-    sortBy: 'most-recent',
-    limit: 4,
-
-    after: updateSlides()
-  });
-  feed.run();
-
-  setInterval(function () {
-    nextPhoto();
-  }, 2000);
-}
-
 function scheduleSlides(hh, mm, ss) {
   var monitorSlidesTime = setInterval(function () {
     var timeNow = new Date();
@@ -85,19 +39,10 @@ function scheduleSlides(hh, mm, ss) {
   }, 1000);
 }
 
-function scheduleInstagram(hh, mm, ss) {
-  var monitorInstagramTime = setInterval(function () {
-    var timeNow = new Date();
-    if (timeNow.getHours() >= hh && timeNow.getMinutes() >= mm) {
-      clearInterval(monitorInstagramTime);
-      stopSlider();
-      startInstagram();
-    }
-  }, 1000);
-}
+
 
 scheduleSlides(19, 09, 0);
-scheduleInstagram(19, 30, 0);
+
 
 function updateSlides() {
   slides = document.getElementsByName('slides');
